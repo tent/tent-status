f5378c5684a9ba2da229a441c74963035e2c8ca6
Jesse Stuart <jessestuart@gmail.com>
2012-11-30 16:31:43 -0500

    Use Sequel instead of DataMapper

--- lib/tent-status/models/user.rb
+++ lib/tent-status/models/user.rb
@@ -1,23 +1,66 @@
 require 'hashie'
-require 'data_mapper'
+require 'sequel'
+require 'yajl'
+require 'sequel/plugins/serialization'
 
 module Tent
   class Status
-    class User
-      include DataMapper::Resource
+    module JsonColumn
+      module Serialize
+        def self.call(value)
+          return value if value.nil? || value.kind_of?(String)
+          Yajl::Encoder.encode(value)
+        end
+      end
 
-      storage_names[:default] = 'users'
+      module Deserialize
+        def self.call(value)
+          return if value.nil?
+          Yajl::Parser.parse(value)
+        end
+      end
+    end
+    Sequel::Plugins::Serialization.register_format(:json, JsonColumn::Serialize, JsonColumn::Deserialize)
 
-      property :id, Serial
-      property :entity, URI
-      property :app_id, String
-      property :app_mac, Json, :default => {}, :lazy => false
-      property :profile, Json, :default => {}
-      property :mac_key_id, String
-      property :mac_key, String
-      property :mac_algorithm, String
-      property :profile_info_types, Json, :default => []
-      property :post_types, Json, :default => []
+    module PGArray
+      module Serialize
+        def self.call(value)
+          return if value.nil?
+          "{#{value.map(&:to_s).map(&:inspect).join(',')}}"
+        end
+      end
+
+      module Deserialize
+        def self.call(value)
+          return if value.nil?
+          value[1..-2].split(',').map { |v| v[0,1] == '"' ? v[1..-2] : v  }
+        end
+      end
+    end
+    Sequel::Plugins::Serialization.register_format(:pg_array, PGArray::Serialize, PGArray::Deserialize)
+
+    DB = Sequel.connect(ENV['DATABASE_URL'], :logger => Logger.new(ENV['DB_LOGFILE'] || STDOUT))
+
+    class User < Sequel::Model(:users)
+      unless DB.table_exists?(:users)
+        DB.create_table :users do
+          primary_key :id
+          column :entity, "text"
+          column :app_id, "text"
+          column :app_mac, "text", :default => "{}"
+          column :profile, "text", :default => "{}"
+          column :mac_key_id, "text"
+          column :mac_key, "text"
+          column :mac_algorithm, "text"
+          column :profile_info_types, "text[]", :default=>"{}"
+          column :post_types, "text[]", :default=>"{}"
+        end
+        User.columns # load columns
+      end
+
+      plugin :serialization
+      serialize_attributes :pg_array, :profile_info_types, :post_types
+      serialize_attributes :json, :app_mac, :profile
 
       def self.find_or_create_from_auth_hash(auth_hash)
         user = first(:entity => auth_hash.uid)
@@ -86,7 +129,4 @@ module Tent
     end
   end
 end
-
-DataMapper.setup(:default, ENV['DATABASE_URL'])
-DataMapper.auto_upgrade!
 
