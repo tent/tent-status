16deb1859d7a3bd0ca36892b3fc932e763ffee8a
Jesse Stuart <jesse@jessestuart.ca>
2013-03-07 15:52:37 -0500

    Make debugging missing assets easier

--- lib/tent-status/sprockets/helpers.rb
+++ lib/tent-status/sprockets/helpers.rb
@@ -1,5 +1,8 @@
 module SprocketsHelpers
+  AssetNotFoundError = Class.new(StandardError)
   def asset_path(source, options = {})
-    "./#{environment.find_asset(source).digest_path}"
+    asset = environment.find_asset(source)
+    raise AssetNotFoundError.new("#{source.inspect} does not exist within #{environment.paths.inspect}!") unless asset
+    "./#{asset.digest_path}"
   end
 end
